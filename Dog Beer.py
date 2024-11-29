31231ce91777fdee72b6991e66b0f17df406bd95
vireetha <dudalaviree69@gmail.com>
2024-11-30 05:25:06 +0530

    oj20

---
+++ Dog Beer.py
@@ -0,0 +1,30 @@
+class Dog:
+
+	
+	animal = 'dog'			
+
+	
+	def __init__(self, breed, color):
+	
+			
+		self.breed = breed
+		self.color = color	
+	
+
+Rodger = Dog("Pug", "brown")
+Buzo = Dog("Bulldog", "black")
+
+print('Rodger details:')
+print('Rodger is a', Rodger.animal)
+print('Breed: ', Rodger.breed)
+print('Color: ', Rodger.color)
+
+print('\nBuzo details:')
+print('Buzo is a', Buzo.animal)
+print('Breed: ', Buzo.breed)
+print('Color: ', Buzo.color)
+
+
+
+print("\nAccessing class variable using class name")
+print(Dog.animal)	
